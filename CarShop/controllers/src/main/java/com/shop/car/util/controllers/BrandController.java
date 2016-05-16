3d26fc57fc66437f0c9131a2309977c79159cf90
tarasenkoDm <lordtda@gmail.com>
2016-05-17 01:04:32 +0300

    add JSON to controller + change *.xml

--- CarShop/controllers/src/main/java/com/shop/car/util/controllers/BrandController.java
+++ CarShop/controllers/src/main/java/com/shop/car/util/controllers/BrandController.java
@@ -1,22 +1,18 @@
-package com.shop.car.util;
+package com.shop.car.util.controllers;
 
 import com.shop.car.Brand;
-import com.sun.javafx.sg.prism.NGShape;
+
+import com.shop.car.util.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.SQLException;
 import java.util.List;
 
-/**
- * @author Tarasenko Dmitriy <lordtda@gmail.com>
- * @version 0.1
- */
 @Controller
 public class BrandController {
 
@@ -24,14 +20,20 @@ public class BrandController {
     private BrandService brandService;
 
     @RequestMapping(value = "/allBrands.html", method = RequestMethod.GET)
-    public ModelAndView listBrands() {
-        List<Brand> brands = brandService.getAllBrands();
-        return new ModelAndView("list-of-brands", "brandListJSP", listBrands());
+    @ResponseBody
+    public List<Brand> getAllBrands() throws SQLException {
+        return getBrands();
     }
 
     @RequestMapping(value = "/deleteBrand", method = RequestMethod.DELETE)
-    public ModelAndView deleteBrand(@RequestParam("brandId") int brandId)throws SQLException{
+    @ResponseBody
+    public List<Brand> deleteBrand(@RequestParam("brandId") int brandId) throws SQLException {
         brandService.deleteBrand(brandId);
-        return new ModelAndView(new RedirectView("allBrands.html"));
+        return getBrands();
+    }
+
+    private List<Brand> getBrands() {
+        List<Brand> brandList = brandService.getAllBrands();
+        return brandList;
     }
 }
---
+++ CarShop/controllers/src/main/java/com/shop/car/util/controllers/CarController.java
@@ -0,0 +1,37 @@
+package com.shop.car.util.controllers;
+
+import com.shop.car.Car;
+import com.shop.car.util.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.sql.SQLException;
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @RequestMapping(value = "/allCars.html", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Car> getAllCars() throws SQLException {
+        return getCars();
+    }
+
+    @RequestMapping(value = "/deleteCar.html", method = RequestMethod.DELETE)
+    @ResponseBody
+    public List<Car> deleteCar(@RequestParam("carId") int carId) throws SQLException {
+        carService.deleteCar(carId);
+        return getCars();
+    }
+
+    private List<Car> getCars() {
+        List<Car> carList = carService.getAllCars();
+        return carList;
+    }
+}
