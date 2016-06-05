9fbb9f11ece0e9a35e8dbb539db132e5e60d27de
tarasenkoDm <lordtda@gmail.com>
2016-06-06 00:21:10 +0300

    create dumps for car table in CarControllerTest(@BeforeClass)

--- CarShop/controllers/src/test/java/com/shop/car/util/controllers/CarControllerTest.java
+++ CarShop/controllers/src/test/java/com/shop/car/util/controllers/CarControllerTest.java
@@ -1,27 +1,20 @@
 package com.shop.car.util.controllers;
 
+import com.google.gson.Gson;
 import com.shop.car.Car;
+import org.apache.log4j.Logger;
 import org.flywaydb.core.Flyway;
-import org.hibernate.SessionFactory;
+import org.json.JSONObject;
 import org.junit.*;
-import org.junit.runner.Result;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import javax.annotation.Resource;
-import javax.sql.DataSource;
-import javax.xml.crypto.Data;
-
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
 
 import static org.junit.Assert.*;
 
-
-//
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration
 //@WebAppConfiguration
@@ -34,53 +27,30 @@ public class CarControllerTest {
     private static String url = "jdbc:postgresql://localhost:5432/bdtachki";
 
 
-    @BeforeClass
-    public static void applayDBSchema() {
+//    @BeforeClass
+//    public static void applayDBSchema() {
+//        final String CARS_DUMP = "C:\\TMP\\dump\\cars_dump.json";
+//        final String CARS_DUMP_AFTER_DELETE = "C:\\TMP\\dump\\cars_dump_after_delete.json";
+//
+//        System.out.println("drop DB");
+//        dropDB();
+//        System.out.println("create all tables");
+//        createSchemaAndTables();
+//        System.out.println("create car's damp");
+//        dumpCarTable(CARS_DUMP);
+//
+//        deleteCarById(5);
+//        dumpCarTable(CARS_DUMP_AFTER_DELETE);
+//
+//        dropDB();
+//        createSchemaAndTables();
+//    }
 
-        dropDB();
-        createSchemaAndTables();
-
-    }
-
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
 
     @Test
     public void getAllCars() throws Exception {
 
-//        List<Car> carList = new LinkedList<>();
-//        Car car = null;
-//
-//        Class.forName(driver);
-//        Connection connection = null;
-//        connection = DriverManager.getConnection(url, user, password);
-//        System.out.println("ok");
-//        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM tachkischema.cars");
-//        ResultSet resultSet = preparedStatement.executeQuery();
-//        int i=0;
-//        while (resultSet.next()){
-//            car = new Car();
-//            car.setCarId(resultSet.getInt("id"));
-//            car.setModel(resultSet.getString("model"));
-//            car.setColor(resultSet.getString("color"));
-//            car.setPrice(resultSet.getInt("price"));
-//            System.out.println(car);
-//            carList.add(car);
-//            System.out.println(i++);
-//        }
-//        System.out.println(carList);
-//        connection.close();
-//
-//        CarController carController = new CarController();
-//        carController.deleteCar(5);
+
 
         int expected = 5;
         int actual = 5;
@@ -91,6 +61,17 @@ public class CarControllerTest {
 
     @Test
     public void deleteCar() throws Exception {
+
+        CarController carController = new CarController();
+        carController.deleteCar(5);
+
+        int expected = 5;
+        int actual = 5;
+
+        assertEquals("Test changeFirstLastWordsInSentence", expected, actual);
+
+
+
 
     }
 
@@ -137,6 +118,54 @@ public class CarControllerTest {
         flyway.setDataSource(url, user, password, null);
         flyway.clean();
         flyway.migrate();
+    }
+
+    private static void dumpCarTable(String dumpName) {
+
+
+
+        JSONObject resultJson = new JSONObject();
+        List<Car> carList = new LinkedList<>();
+
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement("SELECT * FROM tachkischema.cars");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Car car = new Car();
+                car.setCarId(resultSet.getInt("id"));
+                car.setModel(resultSet.getString("model"));
+                car.setColor(resultSet.getString("color"));
+                car.setPrice(resultSet.getInt("price"));
+                carList.add(car);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        resultJson.put("cars", carList);
+        try (FileWriter writer = new FileWriter(dumpName)) {
+            writer.write(resultJson.toString());
+            writer.flush();
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void deleteCarById(int id){
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM tachkischema.cars WHERE id=" + id);
+            preparedStatement.execute();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
